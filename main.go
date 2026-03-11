6f40a1dcc81891cc45c0601282bce2d6965d48b8
gouravmawari <smawari1000@gmail.com>
2026-03-11 07:37:05 +0530

    complete JWT

--- main.go
+++ main.go
@@ -1,49 +1,87 @@
 package main
+
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
-	"encoding/json"
-	"github.com/golang-jwt/jwt/v5"
 	"time"
+	"github.com/golang-jwt/jwt/v5"
 )
+
 type User struct {
 	Name  string `json:"name"`
 	Email string `json:"email"`
 }
 
-var jwtSecret = []byte("my-secret-key");
+var jwtSecret = []byte("my-secret-key")
 
-func generatetoken(username string)(string,error){
-claim := jwt.MapClain{
-	"username":username,
-	"exp":time.Now().Add(time.Hour * 24).Unix()
+func GenerateToken(username string) (string, error) {
+
+	claims := jwt.MapClaims{
+		"username": username,
+		"exp":      time.Now().Add(time.Hour * 24).Unix(),
 	}
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256,claim);
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
 	tokenString, err := token.SignedString(jwtSecret)
-	return token,err
+
+	return tokenString, err
 }
 
-func NewPost(w http.ResponseWriter,r *http.Request){
+func JWTMiddleware(next http.HandlerFunc) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		authHead := r.Header.Get("Authorization")
+
+		if authHead == "" {
+			http.Error(w, "missing header", http.StatusUnauthorized)
+			return
+		}
+
+		tokenString := authHead[7:]
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return jwtSecret, nil
+		})
+
+		if err != nil || !token.Valid {
+			http.Error(w, "Invalid Token", http.StatusUnauthorized)
+			return
+		}
+
+		next(w, r)
+	}
+}
+
+func NewPost(w http.ResponseWriter, r *http.Request) {
+
 	if r.Method != "POST" {
-		http.Error(w,"this method is POST",http.StatusMethodNotAllowed)
+		http.Error(w, "this method is POST", http.StatusMethodNotAllowed)
 		return
 	}
-	var user User;
+
+	var user User
+
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
-		http.Error(w,"invlide JSON",http.StatusBadRequest)
+		http.Error(w, "invalid JSON", http.StatusBadRequest)
 		return
 	}
+
 	w.Header().Set("Content-Type", "application/json")
+
 	json.NewEncoder(w).Encode(user)
 }
 
+func main() {
 
-func main(){
-	http.HandleFunc("/",func(w http.ResponseWriter,r *http.Request){
-		fmt.Fprintf(w ,"this is response");
-	})
-	http.HandleFunc("/user",NewPost);
-	http.ListenAndServe(":4563",nil)
+	http.HandleFunc("/", JWTMiddleware(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "this is response")
+	}))
 
+	http.HandleFunc("/user", NewPost)
+
+	http.ListenAndServe(":4563", nil)
 }
